e48eb2a12fecb37b086b327414433fbf9c98a702
erozhon <43482929+erozhon@users.noreply.github.com>
2020-02-26 15:12:51 -0800

    Re-did correctly this time!

--- stuff.py
+++ stuff.py
@@ -1,24 +1,47 @@
-def greetName(inName):
-    return "Hello, Bob."
+def greet(name):
+   if name is None:
+      return "Hello, my friend."
 
-def greetNull(input):
-    return ("Hello, my friend.")
+   elif isinstance(name, list) == False:
+      if name.isupper() == True and len(name) != 0:
+         return "HELLO " + name + "!"
+      elif len(name) != 0:
+         return "Hello, " + name + "."
 
-def greetYell(input):
-    return "HELLO BOB!"
+   else:
+      capNames = []
+      names = []
+      quoteNames= []
 
-def greetList(input):
-    return "Hello, Bob and Bobby"
+      for n in name:
+         if n.isupper() == True:
+            capNames.append(n)
+         else:
+            if "," in n:
+               if "\"" in n:
+                  n = n.replace('"', '')
+                  names.append(n)
+               else:
+                  namesSplit = n.split(', ')
+                  for l in namesSplit:
+                     names.append(l)
+            else:
+               names.append(n)
+      str = "Hello"
 
-def greetThree(input):
-    return "Hello, b, a, and c"
-
-def greetSome(input):
-    return "Hello, a and b. AND HELLO CAAA"
-
-def greet7(input):
-    return "Hello, Bob, Charlie, and Dianne"
-
-def greetWeird(input):
-    return "Hello, Bob and Charlie, Dianne"
-#for commit
+      for n in names:
+         if (names[-1] == n):
+            if len(names) == 2:
+               str = str + " and "
+            else:
+               str = str + ", and "
+         else:
+            str = str + ", "
+         str = str + n
+      str = str + "."
+      if len(capNames) > 0:
+         str = str + " AND HELLO "
+         for n in capNames:
+            str = str + n
+         str = str + "!"
+      return str
--- test.py
+++ test.py
@@ -1,46 +1,45 @@
 import unittest
 import stuff
-#for commit
 
 class MyTestCase(unittest.TestCase):
     def test_reqOne(self):
         inName = "Bob"
-        output = stuff.greetName(inName)
+        output = stuff.greet(inName)
         expectedout = "Hello, Bob."
         self.assertEqual(output, expectedout)
 
     def test_reqTwo(self):
         input = None
-        out = stuff.greetNull(input)
+        out = stuff.greet(input)
         exout = "Hello, my friend."
         self.assertEqual(out, exout)
 
     def test_reqThree(self):
         input = "BOB"
-        out = stuff.greetYell(input)
+        out = stuff.greet(input)
         exout = "HELLO BOB!"
         self.assertEqual(out, exout)
 
     def test_reqFour(self):
         input = ["Bob", "Bobby"]
-        out = stuff.greetList(input)
-        exout = "Hello, Bob and Bobby"
+        out = stuff.greet(input)
+        exout = "Hello, Bob and Bobby."
         self.assertEqual(out, exout)
 
     def test_reqFive(self):
         input = ["b", "a", "c"]
-        out = stuff.greetThree(input)
-        exout = "Hello, b, a, and c"
+        out = stuff.greet(input)
+        exout = "Hello, b, a, and c."
         self.assertEqual(out, exout)
 
     def test_reqSix(self):
-        self.assertEqual("Hello, a and b. AND HELLO CAAA", stuff.greetSome(["a", "b", "CAAA"]))
+        self.assertEqual("Hello, a and b. AND HELLO CAAA!", stuff.greet(["a", "b", "CAAA"]))
 
     def test_reqSeven(self):
-        self.assertEqual("Hello, Bob, Charlie, and Dianne", stuff.greet7(["Bob", "Charlie, Dianne"]))
+        self.assertEqual("Hello, Bob, Charlie, and Dianne.", stuff.greet(["Bob", "Charlie, Dianne"]))
 
     def test_reqEight(self):
-        self.assertEqual("Hello, Bob and Charlie, Dianne", stuff.greetWeird(["Bob", "\"Charlie, Dianne\""]))
+        self.assertEqual("Hello, Bob and Charlie, Dianne.", stuff.greet(["Bob", "\"Charlie, Dianne\""]))
 
 if __name__ == '__main__':
     unittest.main()
